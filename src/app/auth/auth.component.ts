282921c28f3a4961e0a910be28ff3bf762536fb0
Kim Maida <kim.maida@auth0.com>
2017-10-27 13:11:38 -0400

    Implemented authentication with Auth0

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -5,7 +5,11 @@ import { Component, OnInit } from '@angular/core';
   selector: 'todo-auth',
   template: `
     <div class="todo-auth">
-      auth works!
+      <button *ngIf="!authService.authenticated" (click)="authService.login()" class="btn">Log In</button>
+      <ng-template [ngIf]="authService.authenticated">
+        <img [src]="authService.userProfile?.picture" />{{authService.userProfile?.name}}
+        <button (click)="authService.logout()" class="btn btn-red">Log Out</button>
+      </ng-template>
     </div>
   `,
   styleUrls: ['./auth.component.css']
